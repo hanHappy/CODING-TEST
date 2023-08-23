5c307b9b42981d2eb22cf7c4f46d99dbf4a593dc
hanHappy <hansmin95@gmail.com>
2023-08-23 23:56:47 +0900

    문자열 : 6번(중복 문자 제거)

--- src/inflearn/section_1/Main.java
+++ src/inflearn/section_1/Main.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = "ksekkset";
-        System.out.println(solution(a));
+        System.out.println(solution(sc.next()));
 
     }
 
@@ -14,8 +13,12 @@ public class Main {
         String answer = "";
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < word.length(); i++){
-            String chr = word.
+            String chr = word.substring(i, i+1);
+            int firstFound = word.indexOf(chr);
+            if(firstFound == i)
+                sb.append(chr);
         }
+        answer = sb.toString();
         return answer;
     }
 }
