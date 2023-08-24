d04dd6da5c1b390c75733ce1afd475fcbf55989b
hanHappy <hansmin95@gmail.com>
2023-08-24 11:51:17 +0900

    문자열 : 6번(중복 문자 제거) 보충

--- src/inflearn/section_1/Main.java
+++ src/inflearn/section_1/Main.java
@@ -5,20 +5,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Long start = System.currentTimeMillis();
         System.out.println(solution(sc.next()));
+        Long end = System.currentTimeMillis();
+
+        System.out.printf("소요 시간 : %dms ", end-start);
 
     }
 
+    // 문자열 + 연산
     public static String solution(String word){
         String answer = "";
-        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < word.length(); i++){
-            String chr = word.substring(i, i+1);
-            int firstFound = word.indexOf(chr);
-            if(firstFound == i)
-                sb.append(chr);
+            if(word.indexOf(word.charAt(i))==i)
+                answer += word.charAt(i);
         }
-        answer = sb.toString();
         return answer;
     }
+
+    // StringBuilder 사용
+    // public static String solution(String word){
+    //     String answer = "";
+    //     StringBuilder sb = new StringBuilder();
+    //     for(int i = 0; i < word.length(); i++){
+    //         String chr = word.substring(i, i+1);
+    //         int firstFound = word.indexOf(chr);
+    //         if(firstFound == i)
+    //             sb.append(chr);
+    //     }
+    //     answer = sb.toString();
+    //     return answer;
+    // }
 }
